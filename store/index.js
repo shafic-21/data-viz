a789c6fb41bcb64bbd0d5e4dc4993b87c9793734
GRACENOBLE <gracenoble72@gmail.com>
2024-08-14 16:00:17 +0300

    changed file names

--- store/index.js
+++ store/index.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 export const useFileStore = create((set) => ({
   filePath:
-    "/data/resakss/agricultural-transformation-and-growth/agriculture-value-added-per-worker-(constant-2015-USD).xlsx",
+    "/data/resakss/agricultural-transformation-and-growth/agriculture-value-added-per-worker-constant-2015-USD.xlsx",
   updateFilePath: (filePath) => set(() => ({ filePath })),
 }));
 
